709d39d5dbf41b8aef859c80c9e58c6dc419b019
SergeVR <rsvolc101@gmail.com>
2025-06-23 19:57:16 +0300

    add count_chars and report

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-from stats import get_count_words
+from stats import get_book_text, get_count_chars, get_count_words
+
 
 def main():
-    count = get_count_words()
-    print(f'{count} words found in the document')
+    path: str = 'books/frankenstein.txt'
+    text = get_book_text(path)
+    count_words = get_count_words(text)
+    count_chars = get_count_chars(text)
+    print('============ BOOKBOT ============')
+    print(f'Analyzing book found at {path}...')
+    print('----------- Word Count ----------')
+    print(f'Found {count_words} total words')
+    print('--------- Character Count -------')
+    for char, number in count_chars.most_common():
+        if char.isalpha():
+            print(f'{char}: {number}')
+    print('============= END ===============')
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,9 +1,17 @@
+from collections import Counter
+
+
 def get_book_text(path_to_file: str) -> str:
     with open(path_to_file) as file:
         text = file.read()
     return text
 
 
-def get_count_words(path: str = 'books/frankenstein.txt') -> int:
-    words = get_book_text(path).split()
+def get_count_words(text: str) -> int:
+    words = text.split()
     return len(words)
+
+
+def get_count_chars(text: str) -> dict[str, int]:
+    letters = Counter(text.lower())
+    return letters
