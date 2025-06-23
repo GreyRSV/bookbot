fc57bf354f8989ba2dd85e8ec82ad35c5b4040cd
SergeVR <rsvolc101@gmail.com>
2025-06-23 20:10:21 +0300

    Add docstrings/

--- main.py
+++ main.py
@@ -1,20 +1,14 @@
-from stats import get_book_text, get_count_chars, get_count_words
+"""Программа bookbot считает количество слов и символов в переданном файле."""
+from stats import get_book_text, get_count_chars, get_count_words, print_report
 
 
 def main():
-    path: str = 'books/frankenstein.txt'
+    """Стартовая точка программы."""
+    path = 'books/frankenstein.txt'
     text = get_book_text(path)
     count_words = get_count_words(text)
     count_chars = get_count_chars(text)
-    print('============ BOOKBOT ============')
-    print(f'Analyzing book found at {path}...')
-    print('----------- Word Count ----------')
-    print(f'Found {count_words} total words')
-    print('--------- Character Count -------')
-    for char, number in count_chars.most_common():
-        if char.isalpha():
-            print(f'{char}: {number}')
-    print('============= END ===============')
+    print_report(path, count_words, count_chars)
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,17 +1,36 @@
+"""Вспомогательные функции программы."""
 from collections import Counter
 
 
 def get_book_text(path_to_file: str) -> str:
-    with open(path_to_file) as file:
+    """Получи текст из файла."""
+    with open(path_to_file, encoding='utf-8') as file:
         text = file.read()
     return text
 
 
 def get_count_words(text: str) -> int:
+    """Посчитай количество слов в тексте."""
     words = text.split()
     return len(words)
 
 
-def get_count_chars(text: str) -> dict[str, int]:
+def get_count_chars(text: str) -> Counter[str]:
+    """Посчитай количество символов в тексте."""
     letters = Counter(text.lower())
     return letters
+
+
+def print_report(
+    path: str, count_words: int, count_chars: Counter[str]
+) -> None:
+    """Выведи отчёт о количестве слов и букв в файле."""
+    print('============ BOOKBOT ============')
+    print(f'Analyzing book found at {path}...')
+    print('----------- Word Count ----------')
+    print(f'Found {count_words} total words')
+    print('--------- Character Count -------')
+    for char, number in count_chars.most_common():
+        if char.isalpha():
+            print(f'{char}: {number}')
+    print('============= END ===============')
