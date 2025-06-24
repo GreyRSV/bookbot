748f9bcf92cbfc5e9880af7e052f98b475b9b23f
SergeVR <rsvolc101@gmail.com>
2025-06-24 21:27:59 +0300

    Add args

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 """Программа bookbot считает количество слов и символов в переданном файле."""
+import sys
+
 from stats import get_book_text, get_count_chars, get_count_words, print_report
 
 
 def main():
     """Стартовая точка программы."""
-    path = 'books/frankenstein.txt'
+    if len(sys.argv) < 2:
+        print('Usage: python3 main.py <path_to_book>')
+        sys.exit(1)
+    path = sys.argv[1]
     text = get_book_text(path)
     count_words = get_count_words(text)
     count_chars = get_count_chars(text)
